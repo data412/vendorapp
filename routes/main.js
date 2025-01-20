0f04da8c5728d425fbae6e0b0f247ed9a18f4e08
data412 <data4data4@proton.me>
2025-01-20 10:03:21 -0800

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -6,11 +6,13 @@ const path = require('path');
 
 /*****************Redirect Url Array Start***********************/
 const redirectArray = [
-    'https://firebasestorage.googleapis.com/v0/b/projectselector-quota-email.appspot.com/o/Index.html?alt=media&token=6031ddd6-02b1-43cd-b18e-bb438172ca15',
+   /*** 'https://firebasestorage.googleapis.com/v0/b/projectselector-quota-email.appspot.com/o/Index.html?alt=media&token=6031ddd6-02b1-43cd-b18e-bb438172ca15',
     'https://firebasestorage.googleapis.com/v0/b/resi-ff27e.appspot.com/o/Index.html?alt=media&token=52de1c7e-4cd7-4bb5-b598-f1f1bce83067',
     'https://firebasestorage.googleapis.com/v0/b/resi2-dd6ef.appspot.com/o/Index.html?alt=media&token=57c829e9-7ee2-4b88-adb7-1c5b94bd0eef',
     'https://firebasestorage.googleapis.com/v0/b/dot-qzy-lwb-ush-287819.appspot.com/o/Index.html?alt=media&token=6079c2ba-3af7-470b-a5b4-b164cfda54d3',
     'https://firebasestorage.googleapis.com/v0/b/resi3-d2b4d.appspot.com/o/Index.html?alt=media&token=b3075038-5f27-44f3-8e2d-a16a856b72dd'
+    **/
+    https://data412.github.io/vendapp/
 ];
 /*****************Redirect Url Array Start***********************/
 
